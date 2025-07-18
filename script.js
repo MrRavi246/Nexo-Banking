91fd0cd8aa24cc896bee3c5e86a39efef601ec7c
Kishan-Ramani <kishanramani269@gmail.com>
2025-07-18 17:45:56 +0530

    Enhance index.html by adding new images and a feature section; update script.js for improved GSAP animations and scroll-trigger settings; refine style.css with layout adjustments and new styles for navigation and content sections.

--- script.js
+++ script.js
@@ -23,123 +23,127 @@ document.addEventListener(
   false
 );
 
-function GSAP() {
-  if (window.gsap && window.ScrollTrigger) {
-    gsap.registerPlugin(ScrollTrigger, SplitText);
+ // use a script tag or an external JS file
+ document.addEventListener("DOMContentLoaded", (event) => {
+  // gsap code here!
 
-    function loader() {
-      // to remove scrollbar
-      document.body.style.overflow = "hidden";
-
-      let split = SplitText.create(".con", { type: "chars" });
-
-      let tl = gsap.timeline();
-
-      tl.from(split.chars, {
-        duration: 0.8,
-        y: 100,
-        autoAlpha: 0,
-        stagger: 0.2,
-      });
-
-      tl.to(split.chars, {
-        duration: 0.8,
-        y: -100,
-        autoAlpha: 0,
-        stagger: 0.2,
-      });
-
-      tl.to(".loader", {
+  function GSAP() {
+    if (window.gsap && window.ScrollTrigger) {
+      gsap.registerPlugin(ScrollTrigger, SplitText);
+  
+      function loader() {
+        // to remove scrollbar
+        document.body.style.overflow = "hidden";
+  
+        let split = SplitText.create(".con", { type: "chars" });
+  
+        let tl = gsap.timeline();
+  
+        tl.from(split.chars, {
+          duration: 0.8,
+          y: 100,
+          autoAlpha: 0,
+          stagger: 0.2,
+        });
+  
+        tl.to(split.chars, {
+          duration: 0.8,
+          y: -100,
+          autoAlpha: 0,
+          stagger: 0.2,
+        });
+  
+        tl.to(".loader", {
+          duration: 1,
+          autoAlpha: 0,
+          display: "none",
+          backgroundColor: `rgba(0, 0, 0, 0.77)`,
+        });
+      }
+      loader();
+  
+      function page1() {
+        const tl = gsap.timeline({
+          scrollTrigger: {
+            trigger: ".page1",
+            start: "50% 50%",
+            end: "250% -50%",
+            scrub: 2,
+            pin: `.page1`,
+          },
+        });
+  
+        tl.to(
+          ".content",
+          {
+            rotate: -10,
+            scale: 0.8,
+          },
+          "<"
+        );
+        tl.to(
+          ".imgrow1",
+          {
+            marginBottom: `5vw`,
+          },
+          "<"
+        );
+        tl.to(
+          ".imgrow2",
+          {
+            marginBottom: `20vw`,
+          },
+          "<"
+        );
+  
+        tl.to(
+          ".imgrow3",
+          {
+            marginBottom: `40vw`,
+          },
+          "<"
+        );
+        tl.to(
+          ".imgrow4",
+          {
+            marginBottom: `60vw`,
+          },
+          "<"
+        );
+        tl.to(
+          ".herotext",
+          {
+            opacity: 1,
+            delay: 0.2,
+          },
+          "<"
+        );
+        tl.to(
+          ".content",
+          {
+            backdropFilter: "blur(30px)",
+          },
+          "<"
+        );
+      }
+      page1();
+  
+      function page3(){
+        
+      gsap.to(".page3", {
+        backgroundColor: `white`,
+        color: `black`,
         duration: 1,
-        autoAlpha: 0,
-        display: "none",
-        backgroundColor: `rgba(0, 0, 0, 0.77)`,
-      });
-    }
-    loader();
-
-    function page1() {
-      const tl = gsap.timeline({
         scrollTrigger: {
-          trigger: ".page1",
-          start: "50% 50%",
-          end: "250% -50%",
-          scrub: 2,
-          pin: `.page1`,
+          trigger: `.page2`,
+          start: "90% 10%",
+          end:"100% 50%",
+          scrub: 3,
         },
       });
-
-      tl.to(
-        ".content",
-        {
-          rotate: -10,
-          scale: 0.8,
-        },
-        "<"
-      );
-      tl.to(
-        ".imgrow1",
-        {
-          marginBottom: `5vw`,
-        },
-        "<"
-      );
-      tl.to(
-        ".imgrow2",
-        {
-          marginBottom: `20vw`,
-        },
-        "<"
-      );
-
-      tl.to(
-        ".imgrow3",
-        {
-          marginBottom: `40vw`,
-        },
-        "<"
-      );
-      tl.to(
-        ".imgrow4",
-        {
-          marginBottom: `60vw`,
-        },
-        "<"
-      );
-      tl.to(
-        ".herotext",
-        {
-          opacity: 1,
-          delay: 0.2,
-        },
-        "<"
-      );
-      tl.to(
-        ".content",
-        {
-          backdropFilter: "blur(30px)",
-        },
-        "<"
-      );
+      }
+      page3()
     }
-    page1();
-
-    function page3(){
-      
-    gsap.to(".page3", {
-      backgroundColor: `white`,
-      color: `black`,
-      duration: 1,
-      scrollTrigger: {
-        trigger: `.page2`,
-        start: "80% 10%",
-        end:"100% 50%",
-        scrub: 2,
-        markers:true
-      },
-    });
-    }
-    page3()
   }
-}
-GSAP();
+  GSAP();
+ });
