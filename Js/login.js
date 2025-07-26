7299fc0ddae0ef632620664d21762196f347fdaa
Kishan-Ramani <kishanramani269@gmail.com>
2025-07-26 17:36:32 +0530

    Refactor index.html and feature.html to update navigation links; enhance page4 layout with new structure and styles in style.css; improve login.js and script.js for better authentication handling and user feedback; add new styles in nav.css for hover effects.

--- Js/login.js
+++ Js/login.js
@@ -1,37 +1,37 @@
-// const USER = {
-//   username: "admin",
-//   password: "1234",
-// };
+const USER = {
+  username: "admin",
+  password: "1234",
+};
 
-// function login(e) {
-//   const enteredUser = document.getElementById("account_number").value;
-//   const enteredPass = document.getElementById("password").value;
-//   console.log("enteredUser:", enteredUser);
-//   if (enteredUser === USER.username && enteredPass === USER.password) {
-//     e.preventDefault();
-//     localStorage.clear();
-//     localStorage.setItem("isLoggedIn", "true");
-//     localStorage.setItem("account_number", enteredUser);
+function login(e) {
+  const enteredUser = document.getElementById("account_number").value;
+  const enteredPass = document.getElementById("password").value;
+  console.log("enteredUser:", enteredUser);
+  if (enteredUser === USER.username && enteredPass === USER.password) {
+    e.preventDefault();
+    localStorage.clear();
+    localStorage.setItem("isLoggedIn", "true");
+    localStorage.setItem("account_number", enteredUser);
 
-//     window.location.href = "../index.html";
-//   } else if (
-//     enteredUser === "" ||
-//     enteredPass === "" ||
-//     enteredUser !== USER.account_number ||
-//     enteredPass !== USER.password
-//   ) {
-//     e.preventDefault();
-//     const errorEl = document.createElement("p");
-//     errorEl.innerText = "Invalid account_number or password.";
-//     errorEl.style.color = "red";
-//     document.querySelector(".right-side").appendChild(errorEl);
-//   } else {
-//     //   errorEl.innerText = "Invalid account_number or password.";
-//     console.error("Error");
-//     e.preventDefault();
-//     const errorEl = document.createElement("p");
-//     errorEl.innerText = "Invalid account_number or password.";
-//     errorEl.style.color = "red";
-//     document.querySelector(".right-side").appendChild(errorEl);
-//   }
-// }
+    window.location.href = "../index.html";
+  } else if (
+    enteredUser === "" ||
+    enteredPass === "" ||
+    enteredUser !== USER.account_number ||
+    enteredPass !== USER.password
+  ) {
+    e.preventDefault();
+    const errorEl = document.createElement("p");
+    errorEl.innerText = "Invalid account_number or password.";
+    errorEl.style.color = "red";
+    document.querySelector(".right-side").appendChild(errorEl);
+  } else {
+    //   errorEl.innerText = "Invalid account_number or password.";
+    console.error("Error");
+    e.preventDefault();
+    const errorEl = document.createElement("p");
+    errorEl.innerText = "Invalid account_number or password.";
+    errorEl.style.color = "red";
+    document.querySelector(".right-side").appendChild(errorEl);
+  }
+}
--- Js/script.js
+++ Js/script.js
@@ -1,5 +1,5 @@
 // Clear any existing authentication data
-localStorage.clear();
+// localStorage.clear();
 
 function lenis() {
   // Initialize Lenis
@@ -26,33 +26,266 @@ lenis();
 //   false
 // );
 
-// function middleware() {
+function middleware() {
+  function authMiddleware(next) {
+    const user =
+      localStorage.getItem("isLoggedIn") === "true"
+        ? {
+            account_number: localStorage.getItem("account_number"),
+          }
+        : null;
 
-//   function authMiddleware(next) {
-//     const user =
-//       localStorage.getItem("isLoggedIn") === "true"
-//         ? {
-//             account_number: localStorage.getItem("account_number"),
-//           }
-//         : null;
+    if (user) {
+      console.log("User is authenticated:", user);
+      mainApp();
+      // next();
+    } else {
+      alert("Unauthorized!");
+      window.location.href = "Pages/login.html";
+      localStorage.clear();
+    }
+  }
 
-//     if (user) {
-//       console.log("User is authenticated:", user);
-//       mainApp();
-//       // next();
-//     } else {
-//       alert("Unauthorized!");
-//       window.location.href = "Pages/login.html";
-//       localStorage.clear();
-//     }
-//   }
+  function mainApp() {
+    console.log("User is allowed, app running...");
+    document.addEventListener("DOMContentLoaded", (event) => {
+      // gsap code here!
+      function GSAP() {
+        if (window.gsap && window.ScrollTrigger) {
+          gsap.registerPlugin(ScrollTrigger, SplitText);
 
-//   function mainApp() {
-//     console.log("User is allowed, app running...");
-//   }
+          function loader() {
+            // to remove scrollbar
+            document.body.style.overflow = "hidden";
 
-//   authMiddleware(mainApp);
-// }
+            let split = SplitText.create(".con", { type: "chars" });
+
+            let tl = gsap.timeline();
+
+            tl.to("main", {
+              display: "none",
+            });
+
+            tl.from(split.chars, {
+              duration: 0.8,
+              y: 100,
+              autoAlpha: 0,
+              stagger: 0.2,
+            });
+
+            tl.to(split.chars, {
+              duration: 0.8,
+              y: -100,
+              autoAlpha: 0,
+              stagger: 0.2,
+            });
+
+            tl.to(".loader", {
+              duration: 0.5,
+              autoAlpha: 0,
+              display: "none",
+              backgroundColor: `rgba(0, 0, 0, 0.77)`,
+            });
+
+            tl.to("main", {
+              display: "block",
+              delay: -0.7,
+            });
+          }
+          loader();
+
+          function page1() {
+            const tl = gsap.timeline({
+              scrollTrigger: {
+                trigger: ".page1",
+                start: "50% 50%",
+                end: "250% -50%",
+                scrub: 2,
+                pin: `.page1`,
+              },
+            });
+
+            tl.to(
+              ".content",
+              {
+                rotate: -10,
+                scale: 0.8,
+              },
+              "<"
+            );
+            tl.to(
+              ".imgrow1",
+              {
+                marginBottom: `5vw`,
+              },
+              "<"
+            );
+            tl.to(
+              ".imgrow2",
+              {
+                marginBottom: `20vw`,
+              },
+              "<"
+            );
+
+            tl.to(
+              ".imgrow3",
+              {
+                marginBottom: `40vw`,
+              },
+              "<"
+            );
+            tl.to(
+              ".imgrow4",
+              {
+                marginBottom: `60vw`,
+              },
+              "<"
+            );
+            tl.to(
+              ".herotext",
+              {
+                opacity: 1,
+                delay: 0.2,
+              },
+              "<"
+            );
+            tl.to(
+              ".content",
+              {
+                backdropFilter: "blur(30px)",
+              },
+              "<"
+            );
+          }
+          page1();
+
+          function page3() {
+            gsap.to(".page3", {
+              backgroundColor: `white`,
+              color: `black`,
+              duration: 1,
+              scrollTrigger: {
+                trigger: `.page2`,
+                start: "90% 10%",
+                end: "100% 50%",
+                scrub: 3,
+              },
+            });
+          }
+          page3();
+
+          function page4() {
+            let tl4 = gsap.timeline({
+              scrollTrigger: {
+                trigger: ".part-4",
+                start: "50% 50%",
+                end: "250% 50%",
+                pin: true,
+                scrub: 1,
+              },
+            });
+            tl4.to(
+              ".c-one",
+              {
+                marginTop: "-25%",
+                opacity: "1",
+              },
+              "sct-1"
+            );
+            tl4.to(
+              ".c-two",
+              {
+                opacity: "1",
+              },
+              "sct-2"
+            );
+            tl4.to(
+              ".c-one",
+              {
+                marginTop: "-100",
+                opacity: "0",
+              },
+              "sct-2"
+            );
+            tl4.to(
+              ".c-three",
+              {
+                opacity: "1",
+              },
+              "sct-3"
+            );
+            tl4.to(
+              ".c-two",
+              {
+                opacity: "0",
+              },
+              "sct-3"
+            );
+            tl4.to(
+              ".c-one",
+              {
+                marginTop: "-180%",
+              },
+              "sct-3"
+            );
+            tl4.to(
+              ".c-one",
+              {
+                marginTop: "-230%",
+              },
+              "sct-4"
+            );
+            tl4.to(
+              ".c-three",
+              {
+                opacity: "0",
+              },
+              "sct-4"
+            ),
+              tl4.to(
+                ".c-one",
+                {
+                  marginTop: "-25%",
+                  opacity: "1",
+                },
+                "sct-1"
+              );
+            tl4.to(
+              ".c-two",
+              {
+                opacity: "1",
+              },
+              "sct-2"
+            );
+            tl4.to(
+              ".c-one",
+              {
+                marginTop: "-100",
+                opacity: "0",
+              },
+              "sct-2"
+            );
+            tl4.to(
+              ".cir-part-4",
+              {
+                marginLeft: "100%",
+                rotate: 360,
+              },
+              "sct-4"
+            );
+          }
+          page4();
+        }
+      }
+      GSAP();
+    });
+  }
+
+  authMiddleware(mainApp);
+}
+
+middleware();
 
 // function middleware() {
 //   function authMiddleware(next) {
@@ -87,152 +320,17 @@ lenis();
 //       }, 2000);
 //     }
 //   }
+// }
+//   function mainApp(user) {
+//     console.log("User is allowed, app running with:", user);
+//     // use a script tag or an external JS file
+//
+//   }
 
-  function mainApp(user) {
-    console.log("User is allowed, app running with:", user);
-    // use a script tag or an external JS file
-    document.addEventListener("DOMContentLoaded", (event) => {
-      // gsap code here!
-      function GSAP() {
-        if (window.gsap && window.ScrollTrigger) {
-          gsap.registerPlugin(ScrollTrigger, SplitText);
-    
-          function loader() {
-            // to remove scrollbar
-            document.body.style.overflow = "hidden";
-    
-            let split = SplitText.create(".con", { type: "chars" });
-    
-            let tl = gsap.timeline();
-    
-            tl.to("main", {
-              display: "none",
-            });
-    
-            tl.from(split.chars, {
-              duration: 0.8,
-              y: 100,
-              autoAlpha: 0,
-              stagger: 0.2,
-            });
-    
-            tl.to(split.chars, {
-              duration: 0.8,
-              y: -100,
-              autoAlpha: 0,
-              stagger: 0.2,
-            });
-    
-            tl.to(".loader", {
-              duration: 0.5,
-              autoAlpha: 0,
-              display: "none",
-              backgroundColor: `rgba(0, 0, 0, 0.77)`,
-            });
-    
-            tl.to("main", {
-              display: "block",
-              delay: -0.7,
-            });
-          }
-          loader();
-    
-          function page1() {
-            const tl = gsap.timeline({
-              scrollTrigger: {
-                trigger: ".page1",
-                start: "50% 50%",
-                end: "250% -50%",
-                scrub: 2,
-                pin: `.page1`,
-              },
-            });
-    
-            tl.to(
-              ".content",
-              {
-                rotate: -10,
-                scale: 0.8,
-              },
-              "<"
-            );
-            tl.to(
-              ".imgrow1",
-              {
-                marginBottom: `5vw`,
-              },
-              "<"
-            );
-            tl.to(
-              ".imgrow2",
-              {
-                marginBottom: `20vw`,
-              },
-              "<"
-            );
-    
-            tl.to(
-              ".imgrow3",
-              {
-                marginBottom: `40vw`,
-              },
-              "<"
-            );
-            tl.to(
-              ".imgrow4",
-              {
-                marginBottom: `60vw`,
-              },
-              "<"
-            );
-            tl.to(
-              ".herotext",
-              {
-                opacity: 1,
-                delay: 0.2,
-              },
-              "<"
-            );
-            tl.to(
-              ".content",
-              {
-                backdropFilter: "blur(30px)",
-              },
-              "<"
-            );
-          }
-          page1();
-    
-          function page3() {
-            gsap.to(".page3", {
-              backgroundColor: `white`,
-              color: `black`,
-              duration: 1,
-              scrollTrigger: {
-                trigger: `.page2`,
-                start: "90% 10%",
-                end: "100% 50%",
-                scrub: 3,
-              },
-            });
-          }
-          page3();
-    
-          function page4() {}
-          page4();
-        }
-      }
-      GSAP();
-    });
-  }
+//   function middleware() {
+//     authMiddleware(mainApp);
+//   }
 
-  function middleware() {
-    authMiddleware(mainApp);
-  }
+//   middleware();
 
-  middleware();
-}
-
-middleware();
-
-
+// middleware();
